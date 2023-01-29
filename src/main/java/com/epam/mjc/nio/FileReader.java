b1bb0e004aa829786a5b4527af7104d893689d90
Inesa <112658543+iness02@users.noreply.github.com>
2023-01-29 14:11:27 +0400

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,47 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profileData = new LinkedHashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+                line = br.readLine();
+            }
+
+            String[] parts = stringBuilder.toString().split("\n");
+
+            for (String part : parts) {
+                String[] res = part.split(":");
+                profileData.put(res[0].trim().toLowerCase(), res[1].trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profileBuilder(profileData);
+    }
+
+    private Profile profileBuilder(Map<String, String> profileData) {
+
+        String name = profileData.get("name");
+        int age = Integer.parseInt(profileData.get("age"));
+        String email = profileData.get("email");
+        Long phone = Long.valueOf(profileData.get("phone"));
+
+        return new Profile(name, age, email, phone);
     }
 }
